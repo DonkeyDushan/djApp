04e1db4086bedf164652d39859f22d0fb122a339
Natálie Sedláková <natalie.sedlakova@betrian.cz>
2024-09-20 13:48:20 +0200

    mp3 files

--- src/app/pages/MainPage/sounds.ts
+++ src/app/pages/MainPage/sounds.ts
@@ -2,72 +2,72 @@ import { Howl } from 'howler';
 
 // Funk
 export const bass1 = new Howl({
-  src: [`Funk_Bass1.wav`],
+  src: [`Funk_Bass1.mp3`],
 });
 
 export const bass2 = new Howl({
-  src: ['Funk_Bass2.wav'],
+  src: ['Funk_Bass2.mp3'],
 });
 
 export const drums1 = new Howl({
-  src: ['Funk_Drumz1.wav'],
+  src: ['Funk_Drumz1.mp3'],
 });
 
 export const drums2 = new Howl({
-  src: ['Funk_Drumz2.wav'],
+  src: ['Funk_Drumz2.mp3'],
 });
 
 export const drums3 = new Howl({
-  src: ['Funk_Drumz3.wav'],
+  src: ['Funk_Drumz3.mp3'],
 });
 
 export const keys1 = new Howl({
-  src: ['Funk_Keys1.wav'],
+  src: ['Funk_Keys1.mp3'],
 });
 
 export const keys2 = new Howl({
-  src: ['Funk_Keys2.wav'],
+  src: ['Funk_Keys2.mp3'],
 });
 
 export const keys3 = new Howl({
-  src: ['Funk_Keys3.wav'],
+  src: ['Funk_Keys3.mp3'],
 });
 
 // Anodic
 export const arp1 = new Howl({
-  src: ['Anodic_Arp1.wav'],
+  src: ['Anodic_Arp1.mp3'],
 });
 
 export const arp2 = new Howl({
-  src: ['Anodic_Arp2.wav'],
+  src: ['Anodic_Arp2.mp3'],
 });
 
 export const arp3 = new Howl({
-  src: ['Anodic_Arp3.wav'],
+  src: ['Anodic_Arp3.mp3'],
 });
 
 export const anodicDrums1 = new Howl({
-  src: ['Anodic_Drums1.wav'],
+  src: ['Anodic_Drums1.mp3'],
 });
 
 export const anodicDrums2 = new Howl({
-  src: ['Anodic_Drums2.wav'],
+  src: ['Anodic_Drums2.mp3'],
 });
 
 export const anodicDrums3 = new Howl({
-  src: ['Anodic_Drums3.wav'],
+  src: ['Anodic_Drums3.mp3'],
 });
 
 export const pad1 = new Howl({
-  src: ['Anodic_Pad1.wav'],
+  src: ['Anodic_Pad1.mp3'],
 });
 
 export const pad2 = new Howl({
-  src: ['Anodic_Pad2.wav'],
+  src: ['Anodic_Pad2.mp3'],
 });
 
 export const pad3 = new Howl({
-  src: ['Anodic_Pad3.wav'],
+  src: ['Anodic_Pad3.mp3'],
 });
 
 // Custom
